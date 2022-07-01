50d76f76de85b038388b3265dc2715307f044561
Damdinsvren2020 <56712755+Damdinsvren2020@users.noreply.github.com>
2022-07-01 18:30:44 +0800

    project

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
 // Дэлгэцтэй ажиллах контроллер
-
-var uiController = (function () {})();
-
-// Санхүүтэй ажиллах контроллер
-
-var financeController = (function () {})();
-
-// Программын холбогч контроллер
-
-var appController = (function (uiController, financeController) {
-  var ctrlAddItem = function () {
-    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function () {})();
+
+// Програмын холбогч контроллер
+var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDOMstrings();
+
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+    console.log(uiController.getInput());
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+    // 4. Төсвийг тооцоолно
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+  };
+
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
-    // 1.Оруулах өгөгдлийг дэлгэцээс олж авна
-    // 2.Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална
-    // 3.Олж авсан өгөгдлүүдээ тохирох вэб дээр хадгална
-    // 4.Төсвийг тооцоолно
-    // 5.Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана
   });
 
   document.addEventListener("keypress", function (event) {
-    if (event.key === 13) {
+    if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
     }
   });
